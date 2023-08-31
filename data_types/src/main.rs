219fc8f77408ab81078a29ae7cb033be68fae24d
LuciAkirami <ajsubscriber001@gmail.com>
2023-08-31 22:52:51 +0530

    Adding Data Types in Rust

---
+++ data_types/src/main.rs
@@ -0,0 +1,30 @@
+#![allow(unused)]
+// the file contains all the data types included in rust
+fn main() {
+    // ----- basic data types -------
+    let int_type = 5;         // i32 - 32bit integer by default
+    let float_type = 2.0;         // f64 - 64bit float by default
+    let character_type = 'Z';     // character
+    let boolean_type = true;      // bool data type
+    let float_32_type: f32 = 4.5; // f32 float data type
+
+    // ---- Compound Data Types ----
+    // Tuples - Can contain data of different data types
+    let my_tuple: (u8, char, f32) = (25, 'a', 2.98);
+    let (unsign_int_type, char_type, float_type) = my_tuple; // destructing tuple to individual elemnts
+    let twenty_five = my_tuple.0;               // accessing individual ele
+    println!("Accesing ele in tuple: {}",my_tuple.1);
+
+    // Arrays - Can contain data of single data type
+    let num_array = [1,2,3,4,5];
+    let same_array = [2;5]; // same_array = [2,2,2,2,2]
+    let str_array = ["one","two","three"];
+    println!("Accessing ele in array: {}",num_array[2]);
+
+    // Tuple of Arrays
+    let new_tuple = ([3;5], [9;3]);                  // second_array = [9,9,9]
+    let (first_array,second_array) = new_tuple;      // first_array = [3,3,3,3,3]
+    let array_element_inside_tuple = new_tuple.1[2]; // Accesing third ele of second array
+    println!("Accesing Third Ele of Second Array in Tuple: {}",new_tuple.1[2]);
+
+}
